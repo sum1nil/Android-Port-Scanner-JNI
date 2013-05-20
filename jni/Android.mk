5cbead8cb758b180e7df8d9eee3f8096dfd1e193
sum1nil <ten_wings@hotmail.com>
2013-05-20 15:15:58 -0500

    Update Android.mk
    NDK build files should not be tracked. Added *.mk to .gitignore.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,17 +1,3 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -20,5 +6,6 @@ LOCAL_MODULE    := packetbuilder
 LOCAL_SRC_FILES := PacketBuilder-jni.c
 LOCAL_LDLIBS := -landroid -llog
 LOCAL_STATIC_LIBRARIES := native_app_glue
-LOCAL_C_INCLUDES := /data/data/com.aide.ui/files/ndksupport-20130506/android-ndk-aide/sources/android/native_app_glue /data/data/com.aide.ui/files/ndksupport-20130506/android-ndk-aide/platforms/android-4/arch-arm/usr/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/native_app_glue
+
 include $(BUILD_SHARED_LIBRARY)
