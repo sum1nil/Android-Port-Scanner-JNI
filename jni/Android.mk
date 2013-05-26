d0030fe032addd3cb32732b438f2340d524a030d
Wilson Youmans <ten_wings@hotmail.com>
2013-05-25 20:40:42 -0500

    Writing landscape versions of layouts

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := packetbuilder
 LOCAL_SRC_FILES := PacketBuilder-jni.c
-LOCAL_LDLIBS := -landroid -llog -ldl  
-LOCAL_STATIC_LIBRARIES := native_app_glue ntdll kernel32
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../android-ndk-r8e/toolchains/arm-linux-androideabi-4.7/prebuilt/windows/lib/gcc/arm-linux-androideabi/4.7/include $(LOCAL_PATH)/../../../android-ndk-r8e/sources/android/native_app_glue $(LOCAL_PATH)/../../../android-ndk-r8e/platforms/android-9/arch-arm/usr/include
+LOCAL_LDLIBS := -landroid -llog -ldl
+LOCAL_STATIC_LIBRARIES := native_app_glue c m stdc++
+LOCAL_C_INCLUDES := /data/data/com.aide.ui/files/ndksupport-20130506/android-ndk-aide/sources/android/native_app_glue /data/data/com.aide.ui/files/ndksupport-20130506/android-ndk-aide/platforms/android-4/arch-arm/usr/include /data/data/com.aide.ui/files/ndksupport-20130506/android-ndk-aide/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-arm/lib/gcc/arm-linux-androideabi/4.6/include
 
 include $(BUILD_SHARED_LIBRARY)
 
